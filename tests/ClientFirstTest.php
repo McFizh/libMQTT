5d73c2e8f344223adb73997cec3f7fdb31b4141f
Camilo Sperberg <me@unreal4u.com>
2017-06-09 00:35:41 +0200

    Conversion to PSR-2 Standard
    See http://www.php-fig.org/psr/psr-2/ for more information about PSR-2

--- tests/ClientFirstTest.php
+++ tests/ClientFirstTest.php
@@ -1,59 +1,62 @@
-<?PHP
+<?php
 
 use PHPUnit\Framework\TestCase;
 
-class ClientFirstTest extends TestCase {
+class ClientFirstTest extends TestCase
+{
 
-	public function testUnencryptedClientCreation() {
+    public function testUnencryptedClientCreation()
+    {
 
-		// Try to establish connection to server
-		$client = new LibMQTT\Client("localhost",1883,"phpUnitClient");
+        // Try to establish connection to server
+        $client = new LibMQTT\Client("localhost", 1883, "phpUnitClient");
 
-		//
-		$result = $client->connect();
-		$this->assertTrue($result);
+        //
+        $result = $client->connect();
+        $this->assertTrue($result);
 
-		// Subscribe to 'libmqtt/test' and 'libmqtt/empty' channels
-		$result = $client->subscribe( [
-			"libmqtt/test" => [ "qos" => 1 ],
-			"libmqtt/empty" => [ "qos" => 0 ]
-		] );
-		$this->assertTrue($result);
+        // Subscribe to 'libmqtt/test' and 'libmqtt/empty' channels
+        $result = $client->subscribe([
+            "libmqtt/test" => ["qos" => 1],
+            "libmqtt/empty" => ["qos" => 0]
+        ]);
+        $this->assertTrue($result);
 
-		//
-		$result = $client->publish("libmqtt/test", "testi", 0);
-		$this->assertTrue($result);
+        //
+        $result = $client->publish("libmqtt/test", "testi", 0);
+        $this->assertTrue($result);
 
-		//
-		$client->close();
+        //
+        $client->close();
 
-	}
+    }
 
-	public function testCryptedClientCreation() {
+    public function testCryptedClientCreation()
+    {
 
-		// Try to establish connection to server
-		$client = new LibMQTT\Client("test.mosquitto.org",8883,"phpUnitClient");
-		$client->setCryptoProtocol("tls");
-		$client->setCAFile("tests/mosquitto.org.crt");
+        // Try to establish connection to server
+        $client = new LibMQTT\Client("test.mosquitto.org", 8883, "phpUnitClient");
+        $client->setCryptoProtocol("tls");
+        $client->setCAFile("tests/mosquitto.org.crt");
 
-		//
-		$result = $client->connect();
-		$this->assertTrue($result);
+        //
+        $result = $client->connect();
+        $this->assertTrue($result);
 
-		// Subscribe to 'libmqtt/test' and 'libmqtt/empty' channels
-		$result = $client->subscribe( [
-			"libmqtt/test" => [ "qos" => 1 ],
-			"libmqtt/empty" => [ "qos" => 0 ]
-		] );
-		$this->assertTrue($result);
+        // Subscribe to 'libmqtt/test' and 'libmqtt/empty' channels
+        $result = $client->subscribe([
+            "libmqtt/test" => ["qos" => 1],
+            "libmqtt/empty" => ["qos" => 0]
+        ]);
+        $this->assertTrue($result);
 
-		//
-		$result = $client->publish("libmqtt/test", "testi", 0);
- 		$this->assertTrue($result);
+        //
+        $result = $client->publish("libmqtt/test", "testi", 0);
+        $this->assertTrue($result);
 
-		//
-		$client->close();
+        //
+        $client->close();
 
-	}
+    }
 
 }
--- tests/ClientSecondTest.php
+++ tests/ClientSecondTest.php
@@ -1,72 +1,78 @@
-<?PHP
+<?php
 
 use PHPUnit\Framework\TestCase;
 
-class ClientSecondTest extends TestCase {
+class ClientSecondTest extends TestCase
+{
 
-	private $message1Received = false,
-		$message2Received = false,
-		$message3Received = false;
+    private $message1Received = false,
+        $message2Received = false,
+        $message3Received = false;
 
-	public function testUnencryptedAuthorizedClientCreation() {
+    public function testUnencryptedAuthorizedClientCreation()
+    {
 
-		// Try to establish connection to server
-		$client = new LibMQTT\Client("localhost",1883,"phpUnitClient");
-		$client->setAuthDetails("testuser", "userpass");
+        // Try to establish connection to server
+        $client = new LibMQTT\Client("localhost", 1883, "phpUnitClient");
+        $client->setAuthDetails("testuser", "userpass");
 
-		$result = $client->connect();
-		$this->assertTrue($result);
+        $result = $client->connect();
+        $this->assertTrue($result);
 
-		// Subscribe to 'libmqtt/test' and 'libmqtt/empty' channels
-		$result = $client->subscribe( [
-			"libmqtt/test" => [ "qos" => 1 , "function" => [ $this, "handleReceivedMessages" ] ],
-			"libmqtt/empty" => [ "qos" => 0 , "function" => [ $this, "handleReceivedMessages" ] ],
-			"libmqtt/wcard/+" => [ "qos" => 0 , "function" => [ $this, "handleReceivedMessages" ] ]
-		] );
-		$this->assertTrue($result);
+        // Subscribe to 'libmqtt/test' and 'libmqtt/empty' channels
+        $result = $client->subscribe([
+            "libmqtt/test" => ["qos" => 1, "function" => [$this, "handleReceivedMessages"]],
+            "libmqtt/empty" => ["qos" => 0, "function" => [$this, "handleReceivedMessages"]],
+            "libmqtt/wcard/+" => ["qos" => 0, "function" => [$this, "handleReceivedMessages"]]
+        ]);
+        $this->assertTrue($result);
 
-		// Publish test message with QoS 0 to channel
-		$result = $client->publish("libmqtt/test", "test message 1", 0);
-		$this->assertTrue($result);
+        // Publish test message with QoS 0 to channel
+        $result = $client->publish("libmqtt/test", "test message 1", 0);
+        $this->assertTrue($result);
 
-		// Publish test message with QoS 1 to channel
-		$result = $client->publish("libmqtt/test", "test message 2", 1);
-		$this->assertTrue($result);
-		$this->assertTrue( count( $client->getMessageQueue() ) == 1 );
+        // Publish test message with QoS 1 to channel
+        $result = $client->publish("libmqtt/test", "test message 2", 1);
+        $this->assertTrue($result);
+        $this->assertTrue(count($client->getMessageQueue()) == 1);
 
-		// Publish test message with QoS 0 to channel
-		$result = $client->publish("libmqtt/wcard/test", "test message 3", 0);
-		$this->assertTrue($result);
+        // Publish test message with QoS 0 to channel
+        $result = $client->publish("libmqtt/wcard/test", "test message 3", 0);
+        $this->assertTrue($result);
 
-		// Wait 7 x 40ms for server to send us the previous messages
-		for($l1=0; $l1<7; $l1++)
-		{
-			usleep(40000);
-			$client->eventloop();
-		}
+        // Wait 7 x 40ms for server to send us the previous messages
+        for ($l1 = 0; $l1 < 7; $l1++) {
+            usleep(40000);
+            $client->eventLoop();
+        }
 
-		// By now, we should have received all messages back to us
-		// and also the test message 2 should have been acknowledged
-		$this->assertTrue( count( $client->getMessageQueue() ) == 0 );
-		$this->assertTrue($this->message1Received);
-		$this->assertTrue($this->message2Received);
-		$this->assertTrue($this->message3Received);
+        // By now, we should have received all messages back to us
+        // and also the test message 2 should have been acknowledged
+        $this->assertCount(0, $client->getMessageQueue());
+        $this->assertTrue($this->message1Received);
+        $this->assertTrue($this->message2Received);
+        $this->assertTrue($this->message3Received);
 
-		//
-		$client->close();
+        //
+        $client->close();
 
-	}
+    }
 
-	public function handleReceivedMessages($topic, $msg, $qos) {
-		if( !in_array($topic, array("libmqtt/test","libmqtt/wcard/test") ) )
-			return;
+    public function handleReceivedMessages($topic, $msg, $qos)
+    {
+        if (!in_array($topic, array("libmqtt/test", "libmqtt/wcard/test"))) {
+            return;
+        }
 
-		if($msg == "test message 1" && $qos==0)
-			$this->message1Received = true;
-		if($msg == "test message 2" && $qos==1)
-			$this->message2Received = true;
-		if($msg == "test message 3" && $qos==0)
-			$this->message3Received = true;
-	}
+        if ($msg === "test message 1" && $qos == 0) {
+            $this->message1Received = true;
+        }
+        if ($msg === "test message 2" && $qos == 1) {
+            $this->message2Received = true;
+        }
+        if ($msg === "test message 3" && $qos == 0) {
+            $this->message3Received = true;
+        }
+    }
 
 }

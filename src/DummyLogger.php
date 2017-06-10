6c04d39b5702e985b2fcd4afd34c9ebc0b10ac02
Camilo Sperberg <me@unreal4u.com>
2017-06-10 17:50:15 +0200

    Make everything compatible with really old PHP versions

--- src/DummyLogger.php
+++ src/DummyLogger.php
@@ -12,42 +12,42 @@ use Psr\Log\LogLevel;
  */
 class DummyLogger implements LoggerInterface
 {
-    public function emergency($message, array $context = array()): bool
+    public function emergency($message, array $context = array())
     {
         return $this->log(LogLevel::EMERGENCY, $message, $context);
     }
 
-    public function alert($message, array $context = array()): bool
+    public function alert($message, array $context = array())
     {
         return $this->log(LogLevel::ALERT, $message, $context);
     }
 
-    public function critical($message, array $context = array()): bool
+    public function critical($message, array $context = array())
     {
         return $this->log(LogLevel::CRITICAL, $message, $context);
     }
 
-    public function error($message, array $context = array()): bool
+    public function error($message, array $context = array())
     {
         return $this->log(LogLevel::ERROR, $message, $context);
     }
 
-    public function warning($message, array $context = array()): bool
+    public function warning($message, array $context = array())
     {
         return $this->log(LogLevel::WARNING, $message, $context);
     }
 
-    public function notice($message, array $context = array()): bool
+    public function notice($message, array $context = array())
     {
         return $this->log(LogLevel::NOTICE, $message, $context);
     }
 
-    public function info($message, array $context = array()): bool
+    public function info($message, array $context = array())
     {
         return $this->log(LogLevel::INFO, $message, $context);
     }
 
-    public function debug($message, array $context = array()): bool
+    public function debug($message, array $context = array())
     {
         return $this->log(LogLevel::DEBUG, $message, $context);
     }
@@ -59,7 +59,7 @@ class DummyLogger implements LoggerInterface
      * @param array $context
      * @return bool
      */
-    public function log($level, $message, array $context = array()): bool
+    public function log($level, $message, array $context = array())
     {
         return false;
     }
